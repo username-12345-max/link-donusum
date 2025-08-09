0716ce364d21c5517b6f1007b32cdec19bc999d3
username-12345-max <hakanozmen1989@gmail.com>
2025-08-09 21:10:00 +0300

    Create update.js

---
+++ update.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const sourceUrl = 'https://raw.githubusercontent.com/atakan1983/tvepic/main/mehmet_guncel.m3u';
+
+axios.get(sourceUrl).then(response => {
+  let content = response.data;
+
+  // HTTPS → HTTP dönüşümü
+  content = content.replace(/https:\/\/ottcdn\.kablowebtv\.net/g, 'http://ottcdn.kablowebtv.net');
+
+  // Dosyayı kaydet
+  fs.writeFileSync('mehmet_guncel_modified.m3u', content);
+  console.log('Dosya başarıyla dönüştürüldü!');
+}).catch(error => {
+  console.error('İndirme hatası:', error);
+});
