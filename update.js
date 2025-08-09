b1099aa743381905ef53d55ae260f2d52c389924
username-12345-max <hakanozmen1989@gmail.com>
2025-08-09 22:15:40 +0300

    Update update.js

--- update.js
+++ update.js
@@ -14,24 +14,4 @@ axios.get(sourceUrl).then(response => {
   console.log('Dosya başarıyla dönüştürüldü!');
 }).catch(error => {
   console.error('İndirme hatası:', error);
-});const axios = require('axios');
-const fs = require('fs');
-
-const sourceUrl = 'https://raw.githubusercontent.com/atakan1983/tvepic/main/mehmet_guncel.m3u';
-
-axios.get(sourceUrl).then(response => {
-  let content = response.data;
-
-  // Test: İçeriğin ilk 500 karakterini yazdır
-  console.log('İçerik örneği:', content.slice(0, 500));
-
-  // HTTPS → HTTP dönüşümü
-  content = content.replace(/https:\/\/ottcdn\.kablowebtv\.net/g, 'http://ottcdn.kablowebtv.net');
-
-  // Dosyayı kaydet
-  fs.writeFileSync('mehmet_guncel_modified.m3u', content);
-  console.log('Dosya başarıyla dönüştürüldü!');
-}).catch(error => {
-  console.error('İndirme hatası:', error);
 });
-
