7117b9379b0990d0b397904731c414ab761cb110
username-12345-max <hakanozmen1989@gmail.com>
2025-08-10 12:33:03 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,23 +1,13 @@
 const axios = require('axios');
 const fs = require('fs');
-const cron = require('node-cron');
 
 const sourceUrl = 'https://raw.githubusercontent.com/atakan1983/tvepic/main/mehmet_guncel.m3u';
 
-function guncelle() {
-  axios.get(sourceUrl).then(response => {
-    let content = response.data;
-    content = content.replace(/https:\/\/ottcdn\.kablowebtv\.net/g, 'http://ottcdn.kablowebtv.net');
-    fs.writeFileSync('mehmet_guncel_modified.m3u', content);
-    console.log('✅ Dosya başarıyla dönüştürüldü!');
-  }).catch(error => {
-    console.error('❌ İndirme hatası:', error);
-  });
-}
-
-cron.schedule('30 3 * * *', () => {
-  console.log('⏰ Otomatik güncelleme başlatıldı...');
-  guncelle();
+axios.get(sourceUrl, { timeout: 10000 }).then(response => {
+  let content = response.data;
+  content = content.replace(/https:\/\/ottcdn\.kablowebtv\.net/g, 'http://ottcdn.kablowebtv.net');
+  fs.writeFileSync('mehmet_guncel_modified.m3u', content);
+  console.log('✅ Dosya başarıyla dönüştürüldü!');
+}).catch(error => {
+  console.error('❌ İndirme hatası:', error);
 });
-
-
