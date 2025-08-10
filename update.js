b010436d60261d927b1e1d00610c2ebb28ea6ede
username-12345-max <hakanozmen1989@gmail.com>
2025-08-10 11:30:27 +0300

    zamanlama eklendi

--- update.js
+++ update.js
@@ -1,17 +1,29 @@
 const axios = require('axios');
 const fs = require('fs');
+const cron = require('node-cron');
 
 const sourceUrl = 'https://raw.githubusercontent.com/atakan1983/tvepic/main/mehmet_guncel.m3u';
 
-axios.get(sourceUrl).then(response => {
-  let content = response.data;
+function updatePlaylist() {
+  axios.get(sourceUrl).then(response => {
+    let content = response.data;
 
-  // HTTPS → HTTP dönüşümü (düzeltilmiş regex)
-  content = content.replace(/https:\/\/ottcdn\.kablowebtv\.net/g, 'http://ottcdn.kablowebtv.net');
+    // HTTPS → HTTP dönüşümü
+    content = content.replace(/https:\/\/ottcdn\.kablowebtv\.net/g, 'http://ottcdn.kablowebtv.net');
 
-  // Dosyayı kaydet
-  fs.writeFileSync('mehmet_guncel_modified.m3u', content);
-  console.log('Dosya başarıyla dönüştürüldü!');
-}).catch(error => {
-  console.error('İndirme hatası:', error);
+    // Dosyayı kaydet
+    fs.writeFileSync('mehmet_guncel_modified.m3u', content);
+    console.log(`[${new Date().toLocaleString()}] Dosya başarıyla dönüştürüldü!`);
+  }).catch(error => {
+    console.error(`[${new Date().toLocaleString()}] İndirme hatası:`, error.message);
+  });
+}
+
+// Her gün saat 06:00'da çalıştır
+cron.schedule('0 6 * * *', () => {
+  console.log('Otomatik güncelleme başlatılıyor...');
+  updatePlaylist();
 });
+
+// Manuel çalıştırma için
+updatePlaylist();
